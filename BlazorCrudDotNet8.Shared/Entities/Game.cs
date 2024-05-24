773b616a55a54052a271a7af9d4eec9f6e99a524
Mustafa <mustafa1596315@gmail.com>
2024-05-24 15:25:38 +0300

    Add project files.

---
+++ BlazorCrudDotNet8.Shared/Entities/Game.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorCrudDotNet8.Shared.Entities
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+    }
+}
---
+++ BlazorCrudDotNet8.Shared/Services/IGameService.cs
@@ -0,0 +1,14 @@
+﻿using BlazorCrudDotNet8.Shared.Entities;
+
+namespace BlazorCrudDotNet8.Shared.Services
+{
+    public interface IGameService
+    {
+        Task<List<Game>> GetAllGames();
+        Task<Game> GetGameById(int id);
+        Task<Game> AddGame(Game game);
+        Task<Game> EditGame(int id,Game game);
+        Task<bool> DeleteGame(int id);
+
+    }
+}
